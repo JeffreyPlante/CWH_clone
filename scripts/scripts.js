2280badee9538a32113e1e1313353875bdc64a92
user <jeffdplante@aol.com>
2022-03-04 08:26:54 -0800

    Completed accordion menu function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+//This section creates the mobile-menu button and sidebar function
 const openSidebarButton = document.querySelector('#mobile-menu');
 const closeSidebarButton = document.querySelector('#mobile-menu-killswitch')
 const navSidebar = document.querySelector('.nav-flex');
@@ -11,26 +12,21 @@ closeSidebarButton.addEventListener('click', ()=>{
   navSidebar.classList.remove("sidebar-open");
 });
 
-// Above code works as follows:
-// create variable that stores dom element (to be modified) selected by queryselector
-// create variable that stores dom element (to add listener)
-
-// add listener to that dom element with addeventlistener
-// include code that adds class to the dom element(to be modified)
-
-//HOW TO CREATE THE OPEN/CLOSE BUTTON
-
-//Use querySelectorAll to create a nodelist of all of the buttons
-//iterate over the nodelist to add an event
-//the event must reference the parent container and add the accordion-active class
+//This section creates the accordion cards in the philosophy section
 
 const accordions = document.querySelectorAll('.card');
-console.log(accordions);
+
 accordions.forEach( acc => {
   accordionButton = document.querySelector('acc svg')
-  // console.log('accordionButton')
+
   acc.addEventListener('click', ()=> {
-    acc.classList.add('accordion-active');
+    if (acc.classList.contains('accordion-active')) {
+      acc.classList.remove('accordion-active')
+    }
+    else {
+      acc.classList.add('accordion-active');
+    }
   })
+  
 });
 
