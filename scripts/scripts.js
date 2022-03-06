bb41e32856d77fa3f186cef4c9be5cb861a16ad9
user <jeffdplante@aol.com>
2022-03-06 08:58:52 -0800

    opacity layer finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,19 +2,28 @@
 const openSidebarButton = document.querySelector('#mobile-menu');
 const closeSidebarButton = document.querySelector('#mobile-menu-killswitch')
 const navSidebar = document.querySelector('.nav-menu');
+const opacityLayer = document.querySelector('#opacity-layer');
 
 
 openSidebarButton.addEventListener('click', ()=> {
   if (navSidebar.classList.contains('sidebar-open')) {
     navSidebar.classList.remove('sidebar-open');
+    opacityLayer.classList.remove('opacity-activate');
   }
   else {
     navSidebar.classList.add('sidebar-open');
+    opacityLayer.classList.add('opacity-activate');
   }
 });
 
 closeSidebarButton.addEventListener('click', ()=>{
   navSidebar.classList.remove("sidebar-open");
+  opacityLayer.classList.remove('opacity-activate');
+});
+
+opacityLayer.addEventListener('click', ()=> {
+  navSidebar.classList.remove("sidebar-open");
+  opacityLayer.classList.remove('opacity-activate');
 });
 
 //This section creates the accordion cards in the philosophy section
